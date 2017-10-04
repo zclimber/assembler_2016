d61acffae58c06afad0ef6102d7fe18dfbdc7b1c
zclimber <mywork-olymps@yandex.ru>
2017-10-04 10:22:20 +0300

    Added AVX memcpy

---
+++ assembler/memcpy.h
@@ -0,0 +1,16 @@
+/*
+ * memcpy.h
+ *
+ *  Created on: 4 окт. 2017 г.
+ *      Author: MY
+ */
+
+#ifndef MEMCPY_H_
+#define MEMCPY_H_
+
+#include <cstring>
+#include <immintrin.h>
+
+void memcpy_simple(void* dst, const void* src, std::size_t size);
+void memcpy_avx(void* dst, const void* src, std::size_t size);
+#endif /* MEMCPY_H_ */
