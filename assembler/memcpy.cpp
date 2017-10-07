7519323de60b3162b6150eefb60d7dab51ca9910
zclimber <zclimber@github.com>
2017-10-07 14:43:49 +0300

    Added run_opt - checks with -O2

--- assembler/memcpy.cpp
+++ assembler/memcpy.cpp
@@ -1,5 +1,7 @@
 #include "memcpy.h"
 
+#include <cassert>
+
 void memcpy_simple(void* dst, const void* src, std::size_t size) {
 	char * dst_oth = reinterpret_cast<char *>(dst);
 	const char * src_oth = reinterpret_cast<const char *>(src);
@@ -22,17 +24,22 @@ void memcpy_avx(void* dst, const void* src, std::size_t size) {
 		src = reinterpret_cast<const char *>(src) + df;
 		size -= df;
 	}
-	__m256i * dst_oth = reinterpret_cast<__m256i *>(dst);
-	const __m256i * src_oth = reinterpret_cast<const __m256i *>(src);
-	std::size_t i = 0;
+	__m256i * dst_a = reinterpret_cast<__m256i *>(dst);
+	const __m256i * src_a = reinterpret_cast<const __m256i *>(src);
 	if(reinterpret_cast<unsigned long long>(dst) % 32 == 0){
-		for (; (i + 1) * 32 <= size; i++) {
-			_mm256_store_si256(dst_oth + i, _mm256_load_si256(src_oth + i));
+		for (; 32 <= size;) {
+			_mm256_store_si256(dst_a, _mm256_load_si256(src_a));
+			size -= 32;
+			dst_a++;
+			src_a++;
 		}
 	} else {
-		for (; (i + 1) * 32 <= size; i++) {
-			_mm256_storeu_si256(dst_oth + i, _mm256_load_si256(src_oth + i));
+		for (; 32 <= size;) {
+			_mm256_storeu_si256(dst_a, _mm256_load_si256(src_a));
+			size -= 32;
+			dst_a++;
+			src_a++;
 		}
 	}
-	memcpy_simple(dst_oth + i, src_oth + i, size - i * 32);
+	memcpy_simple(dst_a, src_a, size);
 }
