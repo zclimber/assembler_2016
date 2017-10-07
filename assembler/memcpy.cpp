d269e01fc1bb06b2858fc749fe86d69efb271f0b
zclimber <zclimber@github.com>
2017-10-07 15:14:58 +0300

    Use vmovntdq in memcpy

--- assembler/memcpy.cpp
+++ assembler/memcpy.cpp
@@ -15,7 +15,7 @@ void memcpy_avx(void* dst, const void* src, std::size_t size) {
 		memcpy_simple(dst, src, size);
 		return;
 	}
-	int offs = reinterpret_cast<unsigned long long>(dst) % 32;
+	int offs = reinterpret_cast<unsigned long long>(src) % 32;
 
 	if (offs != 0) {
 		int df = 32 - offs;
@@ -26,16 +26,16 @@ void memcpy_avx(void* dst, const void* src, std::size_t size) {
 	}
 	__m256i * dst_a = reinterpret_cast<__m256i *>(dst);
 	const __m256i * src_a = reinterpret_cast<const __m256i *>(src);
-	if(reinterpret_cast<unsigned long long>(src) % 32 == 0){
+	if(reinterpret_cast<unsigned long long>(dst) % 32 == 0){
 		for (; 32 <= size;) {
-			_mm256_stream_si256(dst_a, _mm256_load_si256(src_a));
+			_mm256_store_si256(dst_a, _mm256_load_si256(src_a));
 			size -= 32;
 			dst_a++;
 			src_a++;
 		}
 	} else {
 		for (; 32 <= size;) {
-			_mm256_stream_si256(dst_a, _mm256_loadu_si256(src_a));
+			_mm256_storeu_si256(dst_a, _mm256_load_si256(src_a));
 			size -= 32;
 			dst_a++;
 			src_a++;
